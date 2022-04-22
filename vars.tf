6f20469966b57480686aecf417c28a6f35c4fb52
JT <101801310+jtforti@users.noreply.github.com>
2022-04-22 23:25:49 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,11 +1,2 @@
 variable "AWS_ACCESS_KEY_ID"{}
 variable "AWS_SECRET_ACCESS_KEY"{}
-
-terraform {
-  cloud {
-    organization = "JT-Demo"
-    workspaces {
-      name = "Terraform-Cloud"
-    }
-  }
-} 
