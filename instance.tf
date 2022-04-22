a917b523dfcbfbe7ee0121b9b68b2013dadb909b
JT <jklingenberg@fortinet.com>
2022-04-22 23:19:17 +0200

    test

--- instance.tf
+++ instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   region = "sa-east-1"
-  access_key=var.AWS_ACCESS_KEY
-  secret_key=var.AWS_SECRET_KEY 
+  access_key=var.AWS_ACCESS_KEY_ID
+  secret_key=var.AWS_SECRET_ACCESS_KEY
 }
 resource "aws_instance" "web" {
   ami           = "ami-02dc8ad50da58fffd"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
--- vars.tf
+++ vars.tf
@@ -1,2 +1,12 @@
-variable "AWS_ACCESS_KEY"{}
-variable "AWS_SECRET_KEY"{} 
+variable "AWS_ACCESS_KEY_ID"{}
+variable "AWS_SECRET_ACCESS_KEY"{}
+
+terraform {
+  cloud {
+    organization = "JT-Demo"
+
+    workspaces {
+      name = "Terraform-Cloud"
+    }
+  }
+} 
